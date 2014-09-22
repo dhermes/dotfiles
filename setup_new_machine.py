f2a941a405c3dbf94519e217d96002ceffe1caff
Danny Hermes <daniel.j.hermes@gmail.com>
2014-09-22 14:55:09 -0700

    Adding rule to enable public key SSH only on Mac OS X.

--- setup_new_machine.py
+++ setup_new_machine.py
@@ -3,6 +3,7 @@ import os
 import platform
 import shutil
 import subprocess
+import sys
 
 
 symlinks = {
@@ -19,8 +20,6 @@ symlinks = {
     '$HOME/dotfiles/ssh_config': '$HOME/.ssh/config',
     '$HOME/dotfiles/Xmodmap': '$HOME/.Xmodmap',
 }
-# os.path.expanduser
-# os.path.expandvars
 aptitude_install = [
     'xclip',
     'xsel',
@@ -45,13 +44,8 @@ pip_install = [
 ]
 
 
-def make_ssh_public_key_only():
-  base_platform = platform.system()
+def _linux_make_ssh_public_key_only():
   # NOTE: This is Linux only.
-  if base_platform != 'Linux':
-    print 'Platform is %r.' % (base_platform,)
-    print 'Exiting make_ssh_public_key_only without doing anything.'
-    return
 
   # See:
   # ('http://www.linux.org/threads/how-to-force-ssh-login-via-'
@@ -86,6 +80,69 @@ def make_ssh_public_key_only():
   subprocess.check_output(['restart', 'ssh'])
 
 
+def _os_x_make_ssh_public_key_only():
+  # NOTE: This is Mac OS X only.
+
+  # See: http://serverfault.com/a/86007
+  ssh_config_fi = '/private/etc/sshd_config'
+  ssh_config_fi_backup = ssh_config_fi + '.factory-defaults'
+
+  # Create a backup.
+  shutil.copyfile(ssh_config_fi, ssh_config_fi_backup)
+
+  with open(ssh_config_fi, 'r') as fh:
+    original_contents = fh.read()
+
+  lines = original_contents.split('\n')
+
+  # Turn PasswordAuthentication off.
+  password_lines = [(i, line) for i, line in enumerate(lines)
+                    if line == '#PasswordAuthentication no']
+  if len(password_lines) != 1:
+    raise ValueError('Non-unique match for PasswordAuthentication.')
+
+  i, line = password_lines[0]
+  do_replace = raw_input('Replace line: %r? [y/N] ' % line)
+  if do_replace.strip().lower() != 'y':
+    raise ValueError('Line rejected by user.')
+
+  lines[i] = 'PasswordAuthentication no'
+
+  # Turn ChallengeResponseAuthentication off.
+  challenge_lines = [(i, line) for i, line in enumerate(lines)
+                     if line == '#ChallengeResponseAuthentication yes']
+  if len(challenge_lines) != 1:
+    raise ValueError('Non-unique match for PasswordAuthentication.')
+
+  i, line = challenge_lines[0]
+  do_replace = raw_input('Replace line: %r? [y/N] ' % line)
+  if do_replace.strip().lower() != 'y':
+    raise ValueError('Line rejected by user.')
+
+  lines[i] = 'ChallengeResponseAuthentication no'
+
+  # Write new lines to file.
+  with open(ssh_config_fi, 'w') as fh:
+    fh.write('\n'.join(lines))
+
+  # NOTE: (Quote from the server fault page)
+  #       "If you are using a stock install (i.e., you didn't build/install
+  #        it yourself from source), launchd should take care of picking up
+  #        the new config without having to restart the daemon."
+
+
+def make_ssh_public_key_only():
+  base_platform = platform.system()
+  # NOTE: This is Linux only.
+  if base_platform == 'Linux':
+    _linux_make_ssh_public_key_only()
+  elif base_platform == 'Darwin':
+    _os_x_make_ssh_public_key_only()
+  else:
+    print 'Platform is %r.' % (base_platform,)
+    print 'Exiting make_ssh_public_key_only without doing anything.'
+
+
 def main():
   for source, symbolic_location in symlinks.iteritems():
     # NOTE: We could make this idempotent by using os.path.islink.
@@ -104,6 +161,6 @@ def main():
 if __name__ == '__main__':
   if getpass.getuser() != 'root':
     print 'Please run as root. This is required to install.'
+    sys.exit(1)
 
   main()
-
