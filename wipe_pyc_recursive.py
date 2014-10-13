a99904b354e1a92859cbeab2974b1a8fbdc3f473
Danny Hermes <daniel.j.hermes@gmail.com>
2014-10-12 23:15:53 -0700

    Making wipe-pyc-recursive work on older version of python.

--- wipe_pyc_recursive.py
+++ wipe_pyc_recursive.py
@@ -9,5 +9,5 @@ for parent, subdirs, files in os.walk(os.getcwd()):
     for fi in files:
         if fi.endswith('.pyc'):
             true_path = os.path.join(parent, fi)
-            print '{} removed'.format(true_path)
+            print '%s removed' % (true_path,)
             os.remove(true_path)
